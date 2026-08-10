9514d8b4f50ee76c2f80dc33bb4f3d8352d7b981
FelipeSa <feliperibeirosa077@gmail.com>
2026-08-10 09:29:26 -0300

    add'

---
+++ MeuApp/MeuApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MeuApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SplashWindow splash = new SplashWindow();
+            splash.Show();
+        }
+    }
+}
---
+++ Tetes/Tetes/Program.cs
@@ -0,0 +1,2 @@
+﻿for (int x = 1; x <= 100; x++)
+Console.WriteLine(x);
