401508af6b49697298c56f715ef3ba3f9675a4b7
FelipeSa <feliperibeirosa077@gmail.com>
2026-08-11 11:27:42 -0300

    d

---
+++ ProjAlimentos/ProjAlimentos/Alimentos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjAlimentos
+{
+    public class Alimentos
+    {
+        public string Nome, Categoria, Peso, Validade, Marca;
+        public double Preco;
+
+        public void ExibirAlimentos()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Categoria: {Categoria}"); 
+            Console.WriteLine($"Peso: {Peso}");
+            Console.WriteLine($"Validade: {Validade}");
+            Console.WriteLine($"Preço: R$ {Preco:F2}");
+        }
+    }
+}
---
+++ ProjAlimentos/ProjAlimentos/Program.cs
@@ -0,0 +1,53 @@
+﻿using ProjAlimentos;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("=== SISTEMA DE CADASTRO DE ALIMENTOS ===");
+Console.ResetColor();
+
+Console.WriteLine("Deseja cadastrar Alimentos (s/n)?");
+
+List<Alimentos> estoque = new List<Alimentos>();
+
+string resposta = Console.ReadLine();
+while (resposta.ToLower() == "s")
+{
+    Alimentos novoAlimento = new Alimentos();
+
+    Console.WriteLine("Digite o nome do alimento: ");
+    novoAlimento.Nome = Console.ReadLine();
+
+    Console.WriteLine("Digite a marca do alimento: ");
+    novoAlimento.Marca = Console.ReadLine();
+
+    Console.WriteLine("Digite a categoria do alimento: ");
+    novoAlimento.Categoria = Console.ReadLine();
+
+    Console.WriteLine("Digite o peso do alimento: ");
+    novoAlimento.Peso = Console.ReadLine();
+
+    Console.WriteLine("Digite a validade do alimento: ");
+    novoAlimento.Validade = Console.ReadLine();
+
+    Console.WriteLine("Digite o valor do alimento: ");
+    novoAlimento.Preco = Convert.ToDouble(Console.ReadLine());
+
+    estoque.Add(novoAlimento);
+
+    Console.Write("Deseja cadastrar outro alimento (s/n)");
+    resposta = Console.ReadLine();
+}
+
+Console.WriteLine("=== ESTOQUE FINAL DE ALIMENTOS ===");
+
+if (estoque.Count > 0)
+{
+    foreach (Alimentos alimeATual in estoque)
+    {
+        alimeATual.ExibirAlimentos();
+    }
+}
+else
+{
+    Console.WriteLine("ESTOQUE VAZIO");
+}
+Console.WriteLine("FIM DA APLICAÇAO");
